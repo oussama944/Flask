d54a766334feb1a3052c95b0eaa370163698be24
GSIB Oussama <Zyed.gsib@gmail.com>
2021-06-22 10:14:40 +0200

    maj 2

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
 Les technos utilisées:
 le framework Flasks( pip install flask-sqlalchemy pip install flask-login)
+Pour sécuriser les mdps dans la database on utilise la fonction hash werkzeug.security 
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,67 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth=Blueprint('auth',__name__)
 
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template("login.html",text="Testing")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Vous etes bien audentifié', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Mdp faux', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html",user=current_user)
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
-    return render_template("sign_up.html")
+    if request.method=='POST':
+        email=request.form.get('email')
+        firstName=request.form.get('firstName')
+        password1=request.form.get('password1')
+        password2=request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('L*email existe déja', category='error')
+        if len(email)<4:
+            flash('Email doit etre plus long que 3',category='error')
+        elif len(firstName)<2:
+            flash('Nom doit etre plus long que 1',category='error')
+        elif password1!=password2:
+            flash('Pas le meme mod de passe',category='error')
+        elif len(password1)<7:
+            flash('MDP trop cour',category='error')
+        else:
+            #add user to database
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            #update database
+            db.session.commit()
+            login_user(user, remember=True)
+            flash('Compte créer',category='success')
+            return redirect(url_for('views.home'))
+    return render_template("sign_up.html",user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
